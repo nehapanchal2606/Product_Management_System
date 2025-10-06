03967c7d0481a1d5d910dbad42b90607fca691c1
Neha Panchal <Nehapanchaltxlab>
2025-10-06 15:16:47 +0530

    Added profile related things

---
+++ ProjectManagement/projects/utils.py
@@ -0,0 +1,12 @@
+
+STATUS_CHOICES = [
+    ('To Do','To Do'),
+    ('In Progress','In Progress'),
+    ('Completed','Completed')
+]
+
+PRIORITY_CHOICES = [
+    ('Low','Low'),
+    ('Medium','Medium'),
+    ('High','High')
+]
--- ProjectManagement/projects/views.py
+++ ProjectManagement/projects/views.py
@@ -1,8 +1,9 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from projects.models import Project
 from django.views.generic import CreateView
 from projects.forms import ProjectForm
 from django.urls import reverse_lazy
+# from notifications.models import Notification
 # Create your views here.
 
 class ProjectCreateView(CreateView):
@@ -10,6 +11,22 @@ class ProjectCreateView(CreateView):
     form_class = ProjectForm
     template_name = "project/project_create.html"
     success_url = reverse_lazy("accounts:dashboard")
+
+    def form_valid(self, form):
+        project = form.save(commit=False)
+        project.owner = self.request.user
+        project.save()
+        return redirect(self.success_url)
+    
+    def get_context_data(self, **kwargs):
+        # latest notifications
+        if self.request.user.is_authenticated:
+            latest_notification = self.request.user.notifications.unread()
+            context = super(ProjectCreateView, self).get_context_data(**kwargs)
+            context['notification_count'] = latest_notification.count()
+            context['latest_notification'] = latest_notification[:3]
+            context['header_text'] = "Dashboard"
+            return context
 
 
 
